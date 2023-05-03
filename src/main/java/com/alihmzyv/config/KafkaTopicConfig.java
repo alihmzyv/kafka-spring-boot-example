e48ef83394e45dc154bbdf02df22b5689c89d27a
alihmzyv <alihmzyv@gmail.com>
2023-05-03 16:46:06 +0400

    Initial commit

---
+++ src/main/java/com/alihmzyv/config/KafkaTopicConfig.java
@@ -0,0 +1,2 @@
+package com.alihmzyv.config;public class KafkaTopicConfig {
+}
---
+++ src/test/java/com/alihmzyv/kafkaspringbootexample/KafkaSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alihmzyv.kafkaspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
